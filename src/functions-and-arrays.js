3159838f06f3256046356857cbb6e3b626dbfb01
Mariana Franco <marianamedeirosfranco@gmail.com>
2024-01-18 11:02:49 +0000

    lab solved

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,87 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num1 < num2) {
+    return num2;
+  } else return num1;
+}
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
-function findLongestWord() {}
+function findLongestWord(wordsArr) {
+  if (wordsArr.length === 0) return 0;
 
+  let longestWord = wordsArr[0];
 
+  for (let i = 1; i < wordsArr.length; i++) {
+    const word = wordsArr[i];
 
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+
+  return longestWord;
+}
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbersArr) {
+  if (!numbersArr) return null;
+  let sum = 0;
 
+  for (let i = 0; i < numbersArr.length; i++) {
+    sum += numbersArr[i];
+  }
 
-
-
+  return sum;
+}
 // Iteration 4 | Numbers Average
-const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
+const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
-
-
+function averageNumbers(numbersArray) {
+  if (numbersArray.length === 0) {
+    return 0;
+  } else {
+    return sumNumbers(numbersArray) / numbersArray.length;
+  }
+}
 
 // Iteration 5 | Find Elements
-const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+const words2 = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
-function doesWordExist() {}
+function doesWordExist(wordsArr, word) {
+  if (wordsArr.length === 0) {
+    return null;
+  }
+
+  let doesExist = false;
+
+  for (let i = 0; i < wordsArr.length; i++) {
+    if (wordsArr[i] === word) {
+      doesExist = true;
+    }
+  }
+
+  return doesExist;
+}
